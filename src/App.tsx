b0a552fd0e28bb12cfbe49d0f01014ed87340fd7
TyperGuy <ray.developper@gmail.com>
2022-07-27 15:57:16 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const WELCOME_STRING = 'This is my model APP';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>{WELCOME_STRING}</h1>
+    </div>
+  );
+}
+
+export default App;
